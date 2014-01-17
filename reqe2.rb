46684070378309eae029af801cafcf1305c00323
tmehta2442 <tmehta@post.harvard.edu>
2014-01-16 23:39:06 -0800

    reqe2

---
+++ reqe2.rb
@@ -0,0 +1,24 @@
+# The following code will fail with NoMethodError : private method 'dream' called for Dog
+
+# Assuming that you cannot modify the source code for the Dog class and that the method
+# must remain private - how can you nonetheless call this method from outside of Dog class?
+## original 
+## Problem is that the method dream is being called as a private method. 
+class Dog
+    def speak
+        puts "woof"
+    end
+
+    private
+
+    def dream
+        puts "chasing a rabbit"
+    end
+end
+
+dog = Dog.new
+dog.speak
+# dog.dream						# will give an error
+dog.send :dream   #the .send seems to be an odd feature. 
+
+
